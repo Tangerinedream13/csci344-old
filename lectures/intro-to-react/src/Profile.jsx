ac609b194399ea8f7b92a1d98ffa2e590238c159
Maria <mariabhaddon@gmail.com>
2024-11-06 08:09:42 -0500

    commit

---
+++ lectures/intro-to-react/src/Profile.jsx
@@ -0,0 +1,8 @@
+import "./Profile.css";
+import React from "react";
+
+export default function Profile({name}) {
+    return (
+        <section className="profile">Hello, {name}!</section>
+    );
+}
